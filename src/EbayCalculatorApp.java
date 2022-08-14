63f1dce7c8ed000c8675031a1745e56dc9353e13
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2022-08-13 17:26:13 -0700

    add itemCost gui items

--- src/EbayCalculatorApp.java
+++ src/EbayCalculatorApp.java
@@ -2,6 +2,8 @@ import gui.EbayCalculatorGUI;
 
 public class EbayCalculatorApp {
     public static void main(String[] args) throws Exception {
+        System.out.println("Starting EbayCalculatorGUI...");
         EbayCalculatorGUI ebayCalculatorGUI = new EbayCalculatorGUI();
+
     }
 }
--- src/gui/EbayCalculatorGUI.java
+++ src/gui/EbayCalculatorGUI.java
@@ -4,6 +4,7 @@ import contstants.CommonConstants;
 import utils.LoadImageService;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class EbayCalculatorGUI extends JFrame {
@@ -12,7 +13,7 @@ public class EbayCalculatorGUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(CommonConstants.FRAME_SIZE[0], CommonConstants.FRAME_SIZE[1]);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
+//        this.setResizable(false);
 
         addGUIItems();
 
@@ -20,13 +21,17 @@ public class EbayCalculatorGUI extends JFrame {
     }
 
     public void addGUIItems() throws Exception{
+        System.out.println("Start adding GUI items");
         addImageLayer();
         addItemPanel();
         addProfitPanel();
+        System.out.println("End adding GUI items");
     }
 
     // Adds the Logo for the GUI
     public void addImageLayer() throws Exception{
+        System.out.println("Start Loading Logo.");
+
         // I am starting to see how important it is to look at someone else's code and understanding. I would honestly recommend going to a
         // decently sized open source program where you can try to learn about the whole process or even just a portion of the process and see
         // how it is coded. You can learn from reading the code
@@ -39,37 +44,56 @@ public class EbayCalculatorGUI extends JFrame {
         }
 
         imageLayer.add(image);
-
         this.getContentPane().add(imageLayer, BorderLayout.NORTH);
 
+        System.out.println("End Loading Logo.");
     }
 
     // Adds GUI Items for Item costs and sold
     public void addItemPanel(){
+        System.out.println("Start adding Item Panel.");
+
+        // Purchase Panel
         JPanel purchaseItem = new JPanel();
-        purchaseItem.setLayout(new GridLayout(2, 2));
+        SpringLayout sprLayout = new SpringLayout();
+        purchaseItem.setLayout(sprLayout);
 
         // Item Cost
-        JPanel itemCost = new JPanel();
         JLabel lItemCost = new JLabel(CommonConstants.ITEM_COST_LABEL);
         JTextField tFItemCost = new JTextField(CommonConstants.INPUT_TEXTFIELD_CHARSIZE);
-        itemCost.add(lItemCost);
-        itemCost.add(tFItemCost);
+
+        purchaseItem.add(lItemCost);
+        purchaseItem.add(tFItemCost);
+
 
         // Item Shipping
-        JPanel itemShippingCost = new JPanel();
         JLabel lItemShipping = new JLabel(CommonConstants.ITEM_SHIPPING_LABEL);
         JTextField tFItemShipping = new JTextField(CommonConstants.INPUT_TEXTFIELD_CHARSIZE);
-        itemShippingCost.add(lItemShipping);
-        itemShippingCost.add(tFItemShipping);
 
-        purchaseItem.add(itemCost);
-        purchaseItem.add(itemShippingCost);
+        purchaseItem.add(lItemShipping);
+        purchaseItem.add(tFItemShipping);
+
+        // Padding Configuration
+        sprLayout.putConstraint(SpringLayout.WEST, lItemCost, 75, SpringLayout.WEST, purchaseItem);
+        sprLayout.putConstraint(SpringLayout.NORTH, lItemCost, 5, SpringLayout.NORTH, purchaseItem);
+        sprLayout.putConstraint(SpringLayout.WEST, tFItemCost, 160, SpringLayout.WEST, purchaseItem);
+        sprLayout.putConstraint(SpringLayout.NORTH, tFItemCost, 5, SpringLayout.NORTH, purchaseItem);
+
+        sprLayout.putConstraint(SpringLayout.WEST, lItemShipping, 75, SpringLayout.WEST, purchaseItem);
+        sprLayout.putConstraint(SpringLayout.NORTH, lItemShipping, 35, SpringLayout.NORTH, purchaseItem);
+        sprLayout.putConstraint(SpringLayout.WEST, tFItemShipping, 160, SpringLayout.WEST, purchaseItem);
+        sprLayout.putConstraint(SpringLayout.NORTH, tFItemShipping, 35, SpringLayout.NORTH, purchaseItem);
         this.getContentPane().add(purchaseItem, BorderLayout.CENTER);
+
+
 
         // Item Sold
         // Item Sold Shipping
 
+        // Selling Panel
+        JPanel sellPanel = new JPanel();
+
+        System.out.println("End adding Item Panel.");
     }
 
     // Adds GUI Items that displays the profit and profit percentage
