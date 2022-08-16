e1f64a2378e0a1a48f7a4ba4222110b3775aa13b
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2022-08-15 19:00:21 -0700

    add bat file to allow jar to be executable

--- src/gui/EbayCalculatorGUI.java
+++ src/gui/EbayCalculatorGUI.java
@@ -8,8 +8,6 @@ import service.SpringLayoutConstraintService;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.text.DecimalFormat;
@@ -26,6 +24,8 @@ public class EbayCalculatorGUI extends JFrame implements KeyListener {
         this.setResizable(false);
 
         addGUIItems();
+
+        this.setVisible(true);
     }
 
     public void addGUIItems() throws Exception{
